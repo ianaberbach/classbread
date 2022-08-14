aaed697ddca5c7cf99c85445bfbef2db3ad2aa34
Ian <naberbach@gmail.com>
2022-08-14 10:29:38 -0400

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -34,14 +34,22 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
-
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-  res.render('show', {
-    bread: Bread[req.params.arrayIndex]
-  })
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
+    })
+  } else {
+    res.render('404')
+  }
 })
 
-
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
 
 module.exports = breads
